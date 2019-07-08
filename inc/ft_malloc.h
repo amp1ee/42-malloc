f70eeca95a66db02f950f48db53b2464cf15e84d
Oleksii Ahieiev <oleksiix.ahieiev@intel.com>
2019-07-08 20:59:39 +0300

    Add block type; Add munmap at the end

--- inc/ft_malloc.h
+++ inc/ft_malloc.h
@@ -4,24 +4,37 @@
 # include <sys/types.h>
 # include <unistd.h>
 # include <sys/mman.h>
+# include <stdbool.h>
 
 # define ALIGN4(x) (((((x) - 1) >> 2) << 2) + 4)
 # define FT_PROT_RW (PROT_READ | PROT_WRITE)
 # define FT_MAP_DEF (MAP_PRIVATE | MAP_ANON)
 # define MIN_NUM_ALLOCS (100)
+# define TINY_BLOCK (1024)
+# define SMALL_BLOCK (64 * TINY_BLOCK)
 
 void                    *g_addr;
 
 typedef int             t_bool;
 
+typedef enum            e_blocktype
+{
+    TINY,
+    SMALL,
+    LARGE
+}                       t_blocktype;
+
 typedef struct          s_meta_blk
 {
     size_t              size;
+    t_blocktype         type;
     t_bool              free;
     struct s_meta_blk   *next;
+    struct s_meta_blk   *prev;
+    void                *data;
 }                       *t_meta_blk;
 
-void            *get_new_block(size_t size);
+void            *get_area(size_t size);
 unsigned        get_pages_amount(size_t page_size, size_t block_size);
 size_t          get_alloc_size(size_t size);
 void            *ft_malloc(size_t size);
--- src/main.c
+++ src/main.c
@@ -1,14 +1,45 @@
 #include "ft_malloc.h"
 
-void            *get_new_block(size_t size)
+void            init_area(void *ptr, size_t res_size, size_t initial_size)
+{
+    t_meta_blk  blk;
+
+    blk = (t_meta_blk)ptr;
+    if (initial_size <= TINY_BLOCK)
+        blk->type = TINY;
+    else if (initial_size <= SMALL_BLOCK)
+        blk->type = SMALL;
+    else
+        blk->type = LARGE;
+    blk->size = res_size - sizeof(struct s_meta_blk);
+    blk->data = blk + sizeof(struct s_meta_blk);
+    blk->free = false;
+    blk->prev = NULL;
+    blk->next = NULL;
+}
+
+void            *allocate_area(size_t size)
+{
+    void        *ptr;
+
+    ptr = mmap(NULL, size, FT_PROT_RW, FT_MAP_DEF, -1, 0);
+    if (ptr == MAP_FAILED)
+        return (NULL);
+    return (ptr);
+}
+
+void            *get_new_area(size_t initial_size)
 {
     void        *ptr;
     size_t      alloc_size;
 
-    alloc_size = get_alloc_size(size);
-    ptr = mmap(NULL, alloc_size, FT_PROT_RW, FT_MAP_DEF, -1, 0);
-    if (ptr == MAP_FAILED)
-        return (NULL);
+    if (initial_size <= SMALL_BLOCK)
+        alloc_size = get_alloc_size(initial_size);
+    else
+        alloc_size = initial_size + sizeof(struct s_meta_blk);
+    ptr = allocate_area(alloc_size);
+    if (ptr != NULL)
+        init_area(ptr, alloc_size, initial_size);
     return (ptr);
 }
 
@@ -19,7 +50,8 @@ unsigned        get_pages_amount(size_t page_size, size_t block_size)
 
     num_allocs = 0;
     num_pages = 0;
-    // if block_size && page_size > 0
+
+    //!!! if block_size && page_size > 0
     while (num_allocs < MIN_NUM_ALLOCS)
     {
         num_pages++;
@@ -44,18 +76,10 @@ void            *ft_malloc(size_t size)
 {
     t_meta_blk  b;
     void        *p;
-//    size_t      blk_size;
 
     b = (t_meta_blk)g_addr;
     if (b == NULL)
-        p = get_new_block(size);
-    
-//    p = NULL;
-//    p = mmap(sizeof(struct s_meta_blk) + size);
-//    if (p == (void *)-1)
-//        return (NULL);
-//    b->size = size;
-//    b->next = p;
+        p = get_new_area(size);    
     return (p);
 }
 
@@ -68,13 +92,14 @@ int         main(int argc, char **argv)
     size_t  size;
 
     if (argc > 1)
-//        size = ALIGN4(atoi(argv[1]));
         size = atoi(argv[1]);
     else
         return -1;
     ptr = ft_malloc(size);
     printf("ptr = %p\n", ptr);
     printf("Pagesize %d\n", getpagesize());
- 
+    printf("Type: %d\n", ((t_meta_blk)ptr)->type);
+    munmap(ptr, ((t_meta_blk)ptr)->size + sizeof(struct s_meta_blk));
+
     return (0);
 }
